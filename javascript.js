1ac15e0954125fc7ef5cbebb6837cbbf672b0ef9
mat09876 <mmcdonnell@cartarwler.com>
2016-05-06 17:27:33 +0100

    update

--- javascript.js
+++ javascript.js
@@ -1,21 +1,29 @@
 var parent = document.querySelector('.wrap');
-var list = [];
+
 var data = [
  ];
+
+var _MONTHS_ = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+var _DAYS_ = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16',
+              '17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
 
 var Item = function(conf) {
     this.ele = document.createElement('div');
     this.ele.className = 'app__months';
     this._data = conf.data;
     this.ele.innerHTML = this._data.month;
+    // this.ele.innerHTML = this._data.days;
 };
+
+function initDaysForMonth(month, year) {
+  return D
+}
 
 function initData(){
   for(var i = 0 ; i < 12 ; i++) {
     data.push({
-      month: ('January','February','March','April','May','June','July','August','September','October','November','December'),
-      value1: ('June'),
-      // name: 'afasd'
+      month: _MONTHS_[i],
+      days: _DAYS_[i]
     });
   }
 }
@@ -28,7 +36,7 @@ function initList() {
   var frag = document.createDocumentFragment();
   for(; i < ln; i++) {
     item = new Item({
-      data: data[i]
+      data: data[i] 
     });
     frag.appendChild(item.ele);
 
